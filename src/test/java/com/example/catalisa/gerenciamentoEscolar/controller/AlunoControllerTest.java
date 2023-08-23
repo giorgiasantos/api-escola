743a77b094f98c7e23bcfacce4a7697efda168e0
giorgiasantos <giorgia.santos@zup.com.br>
2023-08-23 11:26:11 -0300

    Primeiro commit

---
+++ src/test/java/com/example/catalisa/gerenciamentoEscolar/controller/AlunoControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.catalisa.gerenciamentoEscolar.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlunoControllerTest {
+
+    @Test
+    void listarAlunos() {
+    }
+
+    @Test
+    void cadastrarNovoAluno() {
+    }
+
+    @Test
+    void deletarAluno() {
+    }
+}
